ab9c64ca6c3cec4d752da8c1e318d51110bd0f2f
Nilay <54245048+dnilay@users.noreply.github.com>
2020-07-28 14:24:37 +0530

    order service added

---
+++ my-order-service/src/main/java/com/example/myorderservice/MyOrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.myorderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyOrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyOrderServiceApplication.class, args);
+    }
+
+}
